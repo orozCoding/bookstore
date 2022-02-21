91e2075c41449492cd535f65042126c38c4159b9
orozcoding <orozcoding@gmail.com>
2022-02-21 07:16:17 -0400

    install uuid

--- src/components/books/form.js
+++ src/components/books/form.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook, removeBook } from '../../redux/books/books';
+const dispatch = useDispatch();
+
+const submiteBookToStore = () => {
+  const newBook = {
+    
+  }
+}
+
 
 const Form = () => (
   <form>
