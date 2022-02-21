9a1968d4e10ece078f2147ff04ba23aeb3051f94
orozcoding <orozcoding@gmail.com>
2022-02-21 07:05:54 -0400

    re-order components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Books from './redux/books/books';
-import Categories from './redux/categories/categories';
-import Form from './components/form';
+import Books from './components/books/books';
+import Categories from './components/categories';
+import Form from './components/books/form';
 // eslint-disable-next-line import/extensions
 import NavBar from './components/navBar';
 
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,1 +1,14 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import booksReducer from '../components/books/booksReducer';
+
+const reducer = combineReducers({
+  booksReducer,
+});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger),
+);
+
+export default store;
