3ff7507e2d036c218785664cdabce6bca114d499
orozcoding <orozcoding@gmail.com>
2022-02-22 00:40:52 -0400

    add header style

--- src/components/naviBar.js
+++ src/components/naviBar.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => (
-  <header className="d-flex">
-    <p className="navBarLogo">Bookstore</p>
-    <nav className="navBar">
-      <ul>
-        <li key={1}>
-          <NavLink to="/" className={(navData) => (navData.isActive ? 'activeLink' : 'none')}>Books</NavLink>
-        </li>
-        <li key={2}>
-          <NavLink to="/Categories" className={(navData) => (navData.isActive ? 'activeLink' : 'none')}>Categories</NavLink>
-        </li>
-      </ul>
-    </nav>
+  <header id="header" className="d-flex mon">
+    <div id="headerContainer" className="d-flex">
+      <p className="headerLogo">Bookstore CMS</p>
+      <nav id="navBar" className="navBar">
+        <ul id="pagesList" className="d-flex">
+          <li key={1}>
+            <NavLink to="/" className={(navData) => (navData.isActive ? 'activeLink' : 'none')}>BOOKS</NavLink>
+          </li>
+          <li key={2}>
+            <NavLink to="/Categories" className={(navData) => (navData.isActive ? 'activeLink' : 'none')}>CATEGORIES</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <i className="bi bi-person-circle click userLogo" />
+    </div>
   </header>
 );
 
